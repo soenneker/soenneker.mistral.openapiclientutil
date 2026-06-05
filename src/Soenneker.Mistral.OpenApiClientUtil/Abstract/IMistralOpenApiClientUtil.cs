23203d58ff5e66825fb413b9da1d4e7b015e2b36
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:23 -0500

    xml commenting

--- src/Soenneker.Mistral.OpenApiClientUtil/Abstract/IMistralOpenApiClientUtil.cs
+++ src/Soenneker.Mistral.OpenApiClientUtil/Abstract/IMistralOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Mistral.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IMistralOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<MistralOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
